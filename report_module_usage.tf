5b2c2cb803bfc23b41e86f08009fb1f5286568c0
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-03-08 13:03:20 +0200

    Update report_module_usage.tf

--- report_module_usage.tf
+++ report_module_usage.tf
@@ -39,7 +39,7 @@ data "scalr_module_version" "report_get_modver_id" {
   count   = 21
   source  = "${scalr_environment.report_env[count.index].id}/${var.module_name}/${var.module_provider}"
   version = var.module_existing_version_number
-  depends_on = [ null_resource.wait_for_module]
+  #depends_on = [ null_resource.wait_for_module]
 }
 
 resource "scalr_workspace" "report_ws" {
