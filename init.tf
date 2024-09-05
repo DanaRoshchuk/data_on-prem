21c8cf1331ed05a19a7f36e5770f541285591e03
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-09-05 19:11:34 +0300

    Update init.tf

--- init.tf
+++ init.tf
@@ -1,9 +1,16 @@
-terraform {
+/*terraform {
   required_providers {
     scalr = {
       source = "Scalr/scalr" # the latest provider version will be used
     }
   }
-}
+}*/
 
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.main.scalr.dev/scalr/scalr"
+            version= "1.0.0-rc-SCALRCORE-31421"
+        }
+    }
 
