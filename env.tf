43e752f8c58ecd09df84366b830221a72825bf61
danaroshchuk <b.roshchuk@scalr.com>
2025-06-26 14:05:55 +0300

    Update env.tf

--- env.tf
+++ env.tf
@@ -1,6 +1,5 @@
 resource "scalr_environment" "env_tag" {
   name                    = "env_tag_${formatdate("DDMMYYYY", timestamp())}"
-  cost_estimation_enabled = true
   tag_ids                 = [scalr_tag.auto_tag.id]
 }
 
