3b7530c32d81a25523e0154c1d5db754ca34da19
danaroshchuk <b.roshchuk@scalr.com>
2025-06-26 16:18:45 +0300

    Update init.tf

--- init.tf
+++ init.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     scalr = {
-      source  = "registry.scalr.io/scalr/scalr"
-      version = "3.2.0"
+      source = "registry.scalr.io/scalr/scalr" # the latest provider version will be used
     }
   }
 }
